81cc7899b349b889e6c3fa48e3fa95c90c31630e
Tijani Fulani <Fulani07@gmail.com>
2019-05-13 14:50:42 +0100

    yelp App

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+ {
+  name:     'Dishoom',
+  address:   '7 Boundary St, London E2 7JE',
+  phone_number: '0208 534 8753',
+  category:   'chinese',
+ },
+ {
+  name:     'Mushroom',
+  address:   '12 Longlane St, London F5 GH6',
+  phone_number: '0208 534 8753',
+  category:   'french',
+ },
+
+  {
+  name:     'Steakroom',
+  address:   '12 Longlane St, London F5 GH6',
+  phone_number: '0208 534 9678',
+  category:   'belgian',
+ },
+
+  {
+  name:     'Fishroom',
+  address:   '12 Longlane St, London F5 GH6',
+  phone_number: '0208 534 4567',
+  category:   'italian',
+ },
+  {
+  name:     'chickenroom',
+  address:   '12 Longlane St, London F5 GH6',
+  phone_number: '0208 534 1234',
+  category:   'japanese',
+ }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
